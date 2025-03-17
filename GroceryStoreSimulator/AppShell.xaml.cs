0a2b5b1faff44e9a3eb6c2701a13151356fa5c1e
EvansSmith918 <Evans.Smith@usm.edu>
2025-03-17 16:42:37 -0500

    Update AppShell.xaml.cs
    Option but best for routing

--- GroceryStoreSimulator/AppShell.xaml.cs
+++ GroceryStoreSimulator/AppShell.xaml.cs
@@ -5,5 +5,12 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+
+    		// Register routes for programmatic navigation (optional but useful)
+      		Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+       	 	Routing.RegisterRoute(nameof(AislePage), typeof(AislePage));
+        	Routing.RegisterRoute(nameof(ItemPage), typeof(ItemPage));
+        	Routing.RegisterRoute(nameof(CartPage), typeof(CartPage));
+        	Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
 	}
 }
